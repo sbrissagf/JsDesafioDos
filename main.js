4ba9628b389325f9b2175b3dcabbfa98e7f2b78b
Gian Sbrissa <sbrissagf@gmail.com>
2021-11-02 12:08:26 -0300

    desafio dos js

---
+++ main.js
@@ -0,0 +1,9 @@
+/* Desafío número 2, ejemplo 3 */
+let anotherNumber = parseInt(prompt("Ingrese otro número"));
+
+if ((anotherNumber >= 10) && (anotherNumber <= 50)) {
+    alert("Correcto, el número está entre 10 y 50");
+
+} else {
+    alert("Incorrecto, el número no está entre 10 y 50")
+}
